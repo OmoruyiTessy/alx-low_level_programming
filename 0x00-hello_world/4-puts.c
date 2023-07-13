fca75242ab48beb5895e9a98531c94f21ac3daeb
Tessy Ezeali <tessyfona@gmail.com>
2023-07-13 08:11:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * return: always 0
+ * Return: always end 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
