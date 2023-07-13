b668667286269d7b8ea0b7d91248d8b4a9c8f246
Tessy Ezeali <tessyfona@gmail.com>
2023-07-13 07:52:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the the main function
+ *
+ * Return: 0 at the end
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
