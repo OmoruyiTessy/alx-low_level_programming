e2cccb0d726b2410cb9062296c08512121dd3301
Tessy Ezeali <tessyfona@gmail.com>
2023-07-13 07:37:09 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);	
+}
