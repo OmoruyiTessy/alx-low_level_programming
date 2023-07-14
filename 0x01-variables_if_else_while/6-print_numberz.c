bf223e3f0f54c30f1b9e4f351bdcb5b71dd10194
Tessy Ezeali <tessyfona@gmail.com>
2023-07-14 14:21:04 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - it is a function
+ *
+ * Return: ends with 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+
+}
