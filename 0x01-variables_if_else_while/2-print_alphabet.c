af3de1c90d0b468d8e8793b5916186171298cf58
Tessy Ezeali <tessyfona@gmail.com>
2023-07-14 11:12:43 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: ends 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
