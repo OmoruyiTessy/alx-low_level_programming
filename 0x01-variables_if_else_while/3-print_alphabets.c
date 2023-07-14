c6cb0ae3d61a0424df41bc0a2860c341225a59a6
Tessy Ezeali <tessyfona@gmail.com>
2023-07-14 11:35:25 -0700

    upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: ends 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
