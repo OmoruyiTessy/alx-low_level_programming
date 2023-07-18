ade8f8b7e90eae621e4f56f9b76865bdb59b2766
Tessy Ezeali <tessyfona@gmail.com>
2023-07-18 13:54:06 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,7 @@
  *
  * Return: always 0
  */
+
 void print_alphabet_x10(void)
 {
 	char i;
@@ -12,9 +13,8 @@ void print_alphabet_x10(void)
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
-		{
 			_putchar(i);
-		}
 		_putchar('\n');
 	}
+	
 }
