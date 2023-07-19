ac2b393ba26510b01e08e4c2edf12e3596ca60e1
Tessy Ezeali <tessyfona@gmail.com>
2023-07-18 22:16:36 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit
+ * @i: paramater
+ * Return: k
+ */
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
