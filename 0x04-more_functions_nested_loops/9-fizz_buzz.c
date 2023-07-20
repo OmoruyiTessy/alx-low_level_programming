c66206de6b68f7565600ad87a8286c53ee19fa83
Tessy Ezeali <tessyfona@gmail.com>
2023-07-20 14:45:58 -0700

    fissBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - followed by a new line.
+ * Description: But for multiples of three print Fizz
+ * Return: 0 ends
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("fizzBuzz");
+		else if (i % 3 == 0)
+			printf("fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
