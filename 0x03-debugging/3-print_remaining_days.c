f3d2897872e2eebfb9b979155caa79bee38cf9fa
Tessy Ezeali <tessyfona@gmail.com>
2023-07-20 16:56:26 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,6 +22,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
+
 	else
 	{
 		if (month == 2 && day == 60)
@@ -32,6 +33,5 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+		}}
 }
