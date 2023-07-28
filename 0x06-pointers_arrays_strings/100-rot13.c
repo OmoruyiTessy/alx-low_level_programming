7e5acaa205c060e3bd3b2ba975bf473ca85a0fb0
OmoruyiTessy <tessyfona@gmail.com>
2023-07-28 21:48:14 +0000

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - advanced task
+ * @s: encode string
+ * Return: can be 0 or 1
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
