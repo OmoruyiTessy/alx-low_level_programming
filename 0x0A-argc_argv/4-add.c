1fcd59f6137b3e5a3f638319c8b845084de07038
OmoruyiTessy <tessyfona@gmail.com>
2023-08-08 16:28:25 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+/**
+ * is_positive_number - bool
+ * @str: string
+ * Return: 1 or 2
+ */
 bool is_positive_number(const char *str)
 {
-	for (int i = 0; str[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] < '0' || str[i] > '9')
 		{
-		return (false);
+			return (false);
 		}
 	}
-	return true;
+	return (true);
 }
+/**
+ * main - that adds positive number
+ * @argc: count
+ * @argv: vector
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int num;
 
-int main(int argc, char *argv[]) {
-	    int sum = 0;
-
-	        for (int i = 1; i < argc; i++) {
-			        if (is_positive_number(argv[i])) {
-					            int num = atoi(argv[i]);
-						                if (num >= 0) {
-									                sum += num;
-											 } else {
-												                 printf("Error\n");
-														                 return 1;
-																             }
-								        } else {
-										            printf("Error\n");
-											                return 1;
-													        }
-				    }
-
-		    printf("%d\n", sum);
-		        return 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+		{
+			num = atoi(argv[i]);
+			if (num >= 0)
+			{
+				sum += num;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
