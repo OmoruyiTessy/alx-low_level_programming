e9d980eabd1d5c11bffd07d97d063b14fee45ff8
OmoruyiTessy <tessyfona@gmail.com>
2023-08-09 14:02:23 +0000

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	array = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
